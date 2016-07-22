0b79fb6cc0d501dac9da7e9f65326936cad4f76b
Santiago Basulto <santiago.basulto@gmail.com>
2016-07-22 10:14:26 -0300

    Solution

--- calculator/main.py
+++ calculator/main.py
@@ -4,72 +4,99 @@ from calculator.operations import *
 from calculator.exceptions import *
 
 
+def _are_valid_params(params):
+    try:
+        assert isinstance(params, tuple)
+        assert all([isinstance(value, (int, float)) for value in params])
+    except AssertionError:
+        return False
+    return True
+
+
+def _save_in_history(calc, operation, params, result):
+    calc['history'].append(
+        (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), operation, params, result))
+
+
 def create_new_calculator(operations=None):
     """
     Creates a configuration dict for a new calculator. Optionally pre loads an
     initial set of operations. By default a calculator with no operations
-    is created. Returns a dict containing operations(dict) and history(list).
-
+    is created.
     :param operations: Dict with initial operations.
                        ie: {'sum': sum_function, ...}
     """
-    pass
+    if not operations:
+        operations = {}
+    assert isinstance(operations, dict)
+    return {
+        'operations': operations,
+        'history': []
+    }
 
 
 def perform_operation(calc, operation, params):
     """
     Executes given operation with given params. It returns the result of the
     operation execution.
-
     :param calc: A calculator.
     :param operation: String with the operation name. ie: 'add'
     :param params: Tuple containing the list of nums to operate with.
                    ie: (1, 2, 3, 4.5, -2)
     """
-    pass
+    if operation not in get_operations(calc):
+        raise InvalidOperation(
+            '"{}" operation not supported.'.format(operation))
+    if not _are_valid_params(params):
+        raise InvalidParams('Given params are invalid.')
+    result = calc['operations'][operation](*params)
+    _save_in_history(calc, operation, params, result)
+    return result
 
 
 def add_new_operation(calc, operation):
     """
     Adds given operation to the list of supported operations for given calculator.
-
     :param calc: A calculator.
     :param operation: Dict with the single operation to be added.
                       ie: {'add': add_function}
     """
-    pass
+    if not isinstance(operation, dict):
+        raise InvalidOperation('Given operation is invalid.')
+    calc['operations'].update(operation)
 
 
 def get_operations(calc):
     """
     Returns the list of operation names supported by given calculator.
     """
-    pass
+    return calc['operations'].keys()
 
 
 def get_history(calc):
     """
     Returns the history of the executed operations since the last reset or
     since the calculator creation.
-
     History items must have the following format:
         (:execution_time, :operation_name, :params, :result)
-
         ie:
         ('2016-05-20 12:00:00', 'add', (1, 2), 3),
     """
-    pass
+    return calc['history']
 
 
 def reset_history(calc):
     """
     Resets the calculator history back to an empty list.
     """
-    pass
+    calc['history'] = []
 
 
 def repeat_last_operation(calc):
     """
     Returns the result of the last operation executed in the history.
     """
-    pass
+    history = calc['history']
+    if not history:
+        return None
+    return history[-1][-1]
--- calculator/operations.py
+++ calculator/operations.py
@@ -1,23 +1,15 @@
-
 def add(*args):
     # your implementation here
-    pass
+    return sum(args)
 
 def subtract(*args):
     # your implementation here
-    pass
+    return args[0] - sum(args[1:])
 
 def multiply(*args):
     # your implementation here
-    pass
+    return reduce(lambda x, y: x * y, args, 1)
 
 def divide(*args):
     # your implementation here
-    pass
-
-def plot(*args):
-    # OPTIONAL EXTRA CREDIT FUNCTION! 
-    # See README for info.
-    pass
-
-# add your custom operations here
+    return reduce(lambda x, y: float(x) / y, args[1:], args[0])
